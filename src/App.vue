69ae4c2befd89a8bee30f470822870b1d72615df
Masto2008 <117452600+Masto2008@users.noreply.github.com>
2023-09-16 13:23:11 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Transport-Artifact",
           "status": "start"
         },
       ],
